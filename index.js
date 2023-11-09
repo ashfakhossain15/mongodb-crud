083d7355d941d4e97a802285cd30d7881fce4070
Ashfak Hossain <ashfaksikder15@gmail.com>
2023-11-09 13:37:24 +0600

    create user in database

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { header } = require("express/lib/request");
+const port = process.env.PORT || 1200;
+
+app.use(cors());
+app.use(express.json());
+
+// ashfakhossain15
+// @sikder#123@A
+
+const uri =
+  "mongodb+srv://ashfakhossain15:%40sikder%23123%40A@cluster0.lw5jt4z.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try { 
+    await client.connect();
+
+    const database = client.db("usersDB");
+    const usersCollection = database.collection("users");
+
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      console.log("New user", user);
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.log);
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
+
+
+
